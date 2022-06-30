b86c5158ba262a5b187fe59e40dcaeca8cb5f61a
Gabriel Decarli Roquim <47276195+gabrielroquim@users.noreply.github.com>
2022-06-29 21:56:45 -0300

    Update soma.test.js

--- ExercicioEbac/soma.test.js
+++ ExercicioEbac/soma.test.js
@@ -2,6 +2,6 @@ var { soma } = require('../../soma')
 
 describe('Funcionalidade soma', () => {
     it('Valida a soma', () => {
-        expect(soma).toBe(14210)
+        expect(soma).toBe(1233)
     });
 });
